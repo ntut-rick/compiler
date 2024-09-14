0177c35ce74344dd6ec922adce1c8739788ec248
Richardson <t110590016@ntut.edu.tw>
2024-09-15 01:50:10 +0800

    WIP hw0 ex5,7

--- hw0.ml
+++ hw0.ml
@@ -107,3 +107,84 @@ let rec msort l =
 
 let xs = [1;3;2;5;3;9;0;0;4]
 let _ = msort xs
+
+(* ------------------------------------------------------------------------------ *)
+
+(* Exercise 5: Lists *)
+(* (a) square_sum such that square_sum l returns the sum of
+       the square of the integers in the list l. *)
+let square_sum = List.fold_left (fun acc a -> acc + a*a) 0 
+    
+(* (b) find_opt such that find_opt x l returns Some i if the element x appears
+ in the index i of the list l (but not before), and None if x does not appear in the list l *) 
+let rec find_opt x l = 
+  match l with 
+  | []      -> None 
+  | y :: ys -> 
+      if x = y then Some x
+      else find_opt x ys
+
+(* ------------------------------------------------------------------------------ *)
+
+(* Exercise 7: Concatenation *)
+(* Here is a way to code the concatenation of two lists in OCaml. *)
+let rec concat l1 l2 = match l1 with
+  | [] -> l2
+  | x::s -> x :: (concat s l2)
+(* This function, like the @ operator provided by OCaml, has a cost proportional to the length
+   of the first list. In order to be able to perform multiple concatenations without fear of their
+   cost, we propose a new representation of sequences, based on the following data type (which
+   can be seen as a concatenation tree). *)
+type 'a seq =
+  | Elt of 'a
+  | Seq of 'a seq * 'a seq
+(* The concatenation of two sequences s1 and s2 is therefore simply Seq(s1, s2). We can give
+   ourselves a writing shortcut s1 @@ s2 with the definition *)
+let (@@) x y = Seq(x, y)
+(* Such a tree represents a sequence, obtained by considering all its elements in order from left
+   to right. Both trees Seq(Elt 1, Seq(Elt 2, Elt 3)) and Seq(Seq(Elt 1, Elt 2), Elt 3)
+   are the two possible representations of the list [1; 2; 3]. *)
+
+(* (a) hd, tl, mem, rev, map, fold_left, fold_right corresponding to functions of the
+       same name on lists; *)
+let rec hd = function
+  | Elt x      -> x
+  | Seq (x, y) -> hd x
+let rec tl = function
+  | Elt x            -> Elt x
+  | Seq ((Elt x), y) -> y
+  | Seq (x,       y) -> Seq(tl x, y)
+let rec mem a = function
+  | Elt x     -> a = x
+  | Seq (x,y) -> mem a x || mem a y
+let rec rev = function
+  | Elt x     -> Elt x
+  | Seq (x,y) -> Seq (rev y, rev x)
+let rec map f = function
+  | Elt x     -> Elt (f x)
+  | Seq (x,y) -> Seq (map f x,map f y)
+let rec fold_left f init = function
+  | Elt x     -> f init x
+  | Seq (x,y) -> init (* TODO *)
+let rec fold_right f l init =
+  match l with
+  | Elt x     -> f x init
+  | Seq (x,y) -> init (* TODO *) 
+                      
+(* (b) seq2list such that seq2list s returns a OCaml list representing the sequences (do not
+   use @);
+   Bonus (difficult): give a tail recursive version of this function; *)
+let seq2list s = fold_right (fun a acc -> a :: acc) s []
+    
+(* (c) find_opt such that find_opt x l returns Some i if the element x appears at index i in
+       the sequence represented by i (but not before) and None if x does not appear in s; *)
+let rec find_opt' a = function
+  | Elt x    -> if x = a then Some x else None
+  | Seq(x,y) ->
+      match (find_opt' a x, find_opt' a y) with
+      | Some x, _
+      | _, Some x -> Some x
+      | _         -> None
+        
+(* (d) *)
+let nth s n = () (* TODO *)
